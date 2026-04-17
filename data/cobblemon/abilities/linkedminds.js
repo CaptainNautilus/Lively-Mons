15406439469ac9c3504168f1bac493106661a239
Saucecoie <45979667+Saucecoie@users.noreply.github.com>
2026-04-17 05:22:52 -0400

    rehauled dollamb's data, also changed linkedminds so it only works on special moves, but also works when you use a spread move (second hit is weakened 50%)

--- data/cobblemon/abilities/linkedminds.js
+++ data/cobblemon/abilities/linkedminds.js
@@ -1,13 +1,13 @@
 {
     onPrepareHit(source, target, move) {
-      if (move.category === "Status" || move.multihit || move.flags["noparentalbond"] || move.flags["charge"] || move.flags["futuremove"] || move.spreadHit || move.isZ || move.isMax)
+      if (move.category !== "Special" || move.multihit || move.flags["noparentalbond"] || move.flags["charge"] || move.flags["futuremove"] || move.isZ || move.isMax)
         return;
       move.multihit = 2;
       move.multihitType = "linkedminds";
     },
   onSourceModifyDamage(damage, source, target, move) {
     if (move.multihitType === "linkedminds" && move.hit === 2) {
-      return this.chainModify(1);
+      return this.chainModify(0.5);
     }
   },
   onSourceModifySecondaries(secondaries, target, source, move) {
