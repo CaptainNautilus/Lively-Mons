62385db8625d07d6987cf36a8e34edfd38b5cda6
Saucecoie <45979667+Saucecoie@users.noreply.github.com>
2026-05-04 15:56:23 -0400

    did the lang stuff for new moves and abilities

--- data/cobblemon/moves/jetstreamlively.js
+++ data/cobblemon/moves/jetstreamlively.js
@@ -8,6 +8,9 @@
     priority: 0,
     flags: { protect: 1, mirror: 1, metronome: 1, wind: 1 },
     priority: 1,
+    onModifyMove(move, pokemon) {
+		if (pokemon.getStat('atk', false, true) > pokemon.getStat('spa', false, true)) move.category = 'Physical';
+	},
     target: "normal",
     type: "Flying",
     contestType: "Clever"
