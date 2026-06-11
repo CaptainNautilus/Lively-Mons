5cae79e626667d3ce2d11a2d61c4e0c62da94535
Saucecoie <45979667+Saucecoie@users.noreply.github.com>
2026-06-11 12:16:12 -0400

    reworked acoustics

--- data/cobblemon/abilities/acoustics.js
+++ data/cobblemon/abilities/acoustics.js
@@ -1,19 +1,20 @@
 {
-    onFoeTryMove(target, source, move) {
-      const targetAllExceptions = ["perishsong"];
-      if (move.target === "foeSide" || move.target === "all" && !targetAllExceptions.includes(move.id)) {
-        return;
-      }
-      const acousticsHolder = this.effectState.target;
-      if ((source.isAlly(acousticsHolder) || move.target === "all") && move.flags["sound"]) {
-        this.attrLastMove("[still]");
-        this.add("cant", acousticsHolder, "ability: Acoustics", move, "[of] " + target);
-        acousticsHolder.boost({ spa: 1 });
-        return false;
-      }
-    },
-    flags: { breakable: 1 },
-    name: "Acoustics",
-    rating: 3.5,
-    num: 292
+	onTryHit(target, source, move) {
+		if (!move.flags['sound']) return;
+
+		target.boost({spa: 1});
+		this.add('-immune', target, '[from] ability: Acoustics');
+		return null;
+	},
+
+	onAllyTryHit(target, source, move) {
+		if (!move.flags['sound']) return;
+
+		this.add('-immune', target, '[from] ability: Acoustics');
+		return null;
+	},
+	flags: {breakable: 1},
+	name: "Acoustics",
+	rating: 3.5,
+	num: 292,
 }
