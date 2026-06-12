28edd0f08dd5f39bb29359415715a6690a0c88b7
Captain Nautilus <174154100+CaptainNautilus@users.noreply.github.com>
2026-06-12 08:48:57 -0700

    Acoustics fix

--- data/cobblemon/abilities/acoustics.js
+++ data/cobblemon/abilities/acoustics.js
@@ -1,24 +1,15 @@
 {
-	onAnyTryMove(target, source, move) {
-		if (!move.flags['sound']) return;
+onAnyTryHit(target, source, move) {
+	if (!move.flags['sound']) return;
 
-		const holder = this.effectState.target;
+	const holder = this.effectState.target;
 
-		if (target === holder) {
-			this.add('-immune', target, '[from] ability: Acoustic Absorption');
-			this.boost({spa: 1}, holder);
-			return false;
-		}
+	if (target.side !== holder.side) return;
 
-		if (
-			target.side === holder.side &&
-			target !== holder
-		) {
-			this.add('-immune', target, '[from] ability: Acoustic Absorption');
-			this.boost({spa: 1}, holder);
-			return false;
-		}
-	},
+	this.add('-immune', target, '[from] ability: Acoustics');
+	this.boost({spa: 1}, holder);
+	return null;
+},
 	flags: {breakable: 1},
 	name: "Acoustics",
 	rating: 3.5,
