80815b23ec73098508341c8bda1f753bc8a0777d
Saucecoie <45979667+Saucecoie@users.noreply.github.com>
2026-07-08 12:17:23 -0400

    Nyenlives things

---
+++ data/cobblemon/moves/jumpscare.js
@@ -0,0 +1,29 @@
+{
+	num: 10139,
+	accuracy: 100,
+		basePower: 40,
+		category: "Physical",
+		name: "Jumpscare",
+		pp: 10,
+		priority: 3,
+		flags: { contact: 1, protect: 1, mirror: 1, metronome: 1 },
+		onTry(source) {
+			if (source.activeMoveActions > 1) {
+				this.hint("Jumpscare only works on your first turn out.");
+				return false;
+			}
+		},
+		secondaries: [
+			{
+				chance: 100,
+        volatileStatus: 'disable',
+			}, 
+      {
+				chance: 100,
+				volatileStatus: 'flinch',
+			},
+		],
+		target: "normal",
+		type: "Ghost",
+		contestType: "Tough",
+}
