aca87e01d49d9e77b9073de9f47803ac6ae748b0
Saucecoie <45979667+Saucecoie@users.noreply.github.com>
2026-08-31 15:16:48 -0400

    actually fixed scaredy cat I think

--- data/cobblemon/abilities/scaredycat.js
+++ data/cobblemon/abilities/scaredycat.js
@@ -1,22 +1,39 @@
 {
-    onDamagePriority: -30,
-    onDamage(damage, target, source, effect) {
-        if (target.hp === target.maxhp && damage >= target.hp && target.boxFormChanged !== true) {
-            this.add('-ability', target, 'Scaredy Cat');
-            target.hp = 1;
-            this.heal(Math.floor(target.maxhp / 2) - 1, target);
+  onStart(pokemon) {
+    if (
+      pokemon.scaredyCatForme &&
+      pokemon.species.name !== pokemon.scaredyCatForme &&
+      !pokemon.transformed
+    ) {
+      pokemon.formeChange(pokemon.scaredyCatForme, this.effect);
+    }
+  },
+  onDamagePriority: -30,
+  onDamage(damage, target, source, effect) {
+    if (
+      !target.scaredyCatTriggered &&
+      damage >= target.hp
+    ) {
+      target.scaredyCatTriggered = true;
+      this.add("-ability", target, "Scaredy Cat");
 
-            if (target.species.forme === "Nyenlives-Unboxed") {
-                target.formeChange("Nyenlives-Boxed");
-            } else {
-                target.formeChange("Nyenlives-Unboxed");
-            }
-            target.boxFormChanged = true;
-            return 0;
-        }
-    },
-    flags: { breakable: 1 },
-    name: "Scaredy Cat",
-    rating: 3,
-    num: -1,
+      target.sethp(1);
+      const healAmount = Math.floor(target.maxhp / 2) - target.hp;
+      if (healAmount > 0) {
+        this.heal(healAmount, target, target, this.effect);
+      }
+
+      if (target.baseSpecies.baseSpecies === "Nyenlives" && !target.transformed) {
+        target.scaredyCatForme = target.species.id === "nyenlivesunboxed" ?
+          "Nyenlives" : "Nyenlives-Unboxed";
+        target.formeChange(target.scaredyCatForme, this.effect);
+      }
+
+      return 0;
+    }
+  },
+  flags: { breakable: 1 },
+  name: "Scaredy Cat",
+  rating: 3,
+  num: -1
 }
